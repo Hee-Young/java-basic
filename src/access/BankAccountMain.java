9d2b89f6078b745f5b24299331f7a6f9bf36623f
Hee-Young <kkhy991222@gmail.com>
2024-07-08 23:57:01 +0900

    feat: 캡슐화

---
+++ src/access/BankAccountMain.java
@@ -0,0 +1,11 @@
+package access;
+
+public class BankAccountMain {
+
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount();
+        account.deposit(10000);
+        account.withdraw(1000);
+        System.out.println("balace = " + account.getBalance());
+    }
+}
